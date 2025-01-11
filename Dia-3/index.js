23582cff5a7918e7792e3ad1be4e4c438f92e32c
Bruno Moraes <brunoleovds@gmail.com>
2025-01-11 10:37:53 -0300

    Fazendo algumas modificações

--- Dia-3/index.js
+++ Dia-3/index.js
@@ -1,5 +1,9 @@
 var seuNome = prompt('Qual o seu nome ?')
-var suaIdade = prompt('Qual a sua idade ?')
-var suaAltura = prompt('Qual sua altura ?')
-var seuPeso = prompt('Qual o seu peso ?')
+var suaIdade = Number.prompt('Qual a sua idade ?')
+var suaAltura = parseFloat(prompt('Qual sua altura ?'))
+var seuPeso = parseInt(prompt('Qual o seu peso ?'))
 
+var anoNas = 2023 - suaIdade
+var Imc = seuPeso / (suaAltura * suaAltura)
+
+console.log('Olá ' + seuNome +', voce tem ' + suaIdade + ' e nasceu em ' + anoNas + ', tem ' + suaAltura + ' pesa ' + seuPeso + ' e seu IMC é de ' + Imc + ' Kg/m²')
