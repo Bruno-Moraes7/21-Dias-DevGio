be4966e9d8da63e89faa195dc330b22ec763a24e
Bruno Moraes <brunoleovds@gmail.com>
2025-01-12 20:28:04 -0300

    Aprendendo && e OU

--- Dia-4/index.js
+++ Dia-4/index.js
@@ -1,7 +1,10 @@
 let idade = Number(prompt('Insira a sua idade'))
-
-if(idade > 18){
-    console.log('Você é maior de idade')
+let nome = prompt('Qual o seu nome ?')
+if(idade === 16 && nome === "Bruno"){
+    console.log('Você tem 16 anos e seu nome é Bruno')
+}else if(idade === 16 || nome === "Bruno"){
+    console.log('Ou voce tem 16 anos ou se chama bruno')
 }else{
-    console.log('Você é menor de idade')
+    console.log('Você não tem 16 anos e não se chama bruno')
 }
+
