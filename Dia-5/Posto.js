b1892c8da8006aa5d8a4814661e91fedb844ebd5
Bruno Moraes <brunoleovds@gmail.com>
2025-01-13 10:51:15 -0300

    Tudo certo com o código, precisa abastecer ?

--- Dia-5/Posto.js
+++ Dia-5/Posto.js
@@ -1,3 +1,18 @@
+let valor 
+let quantidade 
 let opcao = prompt('Olá, selecione a opção desejada:' + '\n1 = Gasolina; \n2 = Álcool; \n3 = Calibrar')
-let valor = parseInt(prompt('Qual o valor que irá pagar ?'))
-
+switch(opcao){
+    case "1":
+        valor = parseInt(prompt("Digíte aqui o valor que irá usar"))
+        quantidade = valor / 5
+        console.log('Você abasteceu ' + quantidade + 'L de gasolina')
+        break;
+    case "2":
+        valor = parseInt(prompt("Digíte aqui o valor que irá usar"))
+        quantidade = valor / 3
+        console.log('Você abasteceu ' + quantidade + 'L de Álcool')
+        break;
+    default:
+        console.log('Pneus calibrados com sucesso')
+        break;
+}
