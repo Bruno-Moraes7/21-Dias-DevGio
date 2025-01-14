92e16733dd6938a1ad2fef5b2191554b6c53ea06
Bruno Moraes <brunoleovds@gmail.com>
2025-01-14 10:54:57 -0300

    Somando 5

--- Dia-6/index.js
+++ Dia-6/index.js
@@ -3,3 +3,8 @@ let num = parseInt(prompt('Digite um número inteiro e positivo'))
 for(let i = 0; i <= num; i++){
     console.log(i)
 }
+
+
+for(let i = 0; i <= 50; i +=5 ){
+    console.log('i')
+}
