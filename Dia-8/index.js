7610dba21e6d123baa3cb3630a25232ce5843e7f
Bruno Moraes <brunoleovds@gmail.com>
2025-02-13 20:42:01 -0300

    Praticando algumas coisas

---
+++ Dia-8/index.js
@@ -0,0 +1,9 @@
+//Praticando.....
+
+// Função que exibe uma saudação no console
+function mostrarSaudacao() {
+    console.log("Olá, seja bem-vindo ao JavaScript!");
+}
+
+// Chama a função para mostrar a saudação
+mostrarSaudacao();
